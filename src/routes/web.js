8dcb54ec51f7099d55d47da2fdd320c4b2f1bc34
dinhtho2001 <phamdinhtho.0105@gmail.com>
2023-02-27 21:29:59 +0700

    configuration MVC

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,13 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
 
-    router.get('/', (req, res) => {
-        return res.send('Hello word')
-    });
-    
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
+
     return app.use("/", router);
 }
 
